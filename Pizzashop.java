078560ae310c1768386c682e8a9f60925c2a976d
ShubhamTigane <shubhamtigane.st@gmail.com>
2023-04-20 14:34:57 +0530

    Message

--- Pizzashop.java
+++ Pizzashop.java
@@ -1,4 +1,3 @@
-package assignmentp;
 
 import java.util.Scanner;
 
@@ -8,7 +7,7 @@ public class Pizzashop {
 		// TODO Auto-generated method stub
     Scanner sc = new Scanner(System.in);
     int amount=0,choice,qty,total=0;
-    System.out.println("1.veg pizza - 100rs 2.panner pizza - 200rs 3.corn pizza 150rs  4. regular pizaa+combopack  300rs 5.to bill 6.exit");
+    System.out.println("1.veg pizza - 100rs \n2.panner pizza - 200rs \n3.corn pizza 150rs  \n4.regular pizza+combo pack  300rs \n5.Total \n6.exit");
 	do {
 		System.out.println("enter the choice");
 	 choice=sc.nextInt();
@@ -19,39 +18,39 @@ public class Pizzashop {
 		System.out.println("enter the qty of veg pizza");
 		 qty=sc.nextInt();
 		amount=amount+qty*100;
-	System.out.println("amount for"+qty+"pizzza is="+amount);
+	System.out.println("amount for "+qty+" pizzza is= "+amount);
 	break;
 	
 	case 2:
 		System.out.println("enter the qty for pannner pizza");
 		qty=sc.nextInt();
 		amount=amount+ qty*200;
-		System.out.println("amount for"+qty+"pizza="+amount);
+		System.out.println("amount for "+qty+" pizza= "+amount);
 		break;
 		
 	case 3:
 		System.out.println("enter the qty for corn pizza");
 		qty=sc.nextInt();
 		amount=amount+ qty*150;
-		System.out.println("amount for"+qty+"pizza="+amount);
+		System.out.println("amount for"+qty+" pizza="+amount);
 		break;
 		
 	case 4:
 		System.out.println("enter the qty for regular pizza");
 		qty=sc.nextInt();
 		amount=amount+ qty*300;
-		System.out.println("amount for"+qty+"pizza="+amount);
+		System.out.println("amount for "+qty+"pizza= "+amount);
 		break;
 		
 		
 	case 5:
-		System.out.println("total amount="+amount);
-		System.out.println("------thank you-----------");
-		System.out.println("------come again----------");
+		System.out.println("Total Amount="+amount);
+		System.out.println("------Thank You-----------");
+		System.out.println("------Come Again----------");
 		break;
 	
 	default:
-		System.out.println("invaild choice");
+		System.out.println("invaild choice!!!");
 	break;
 	}
 	
